137b11a78ed554f843c0c5becb1567bae71b4174
Joey <jaschuett@wisc.edu>
2021-10-30 22:58:51 -0500

    Implemented a method which finds connected stones given a stone, and a bunch of helper methods

--- Go/Shared/Models/Board.cs
+++ Go/Shared/Models/Board.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Collections;
 
 namespace Go.Shared.Models
 {
@@ -60,43 +61,129 @@ namespace Go.Shared.Models
         {
             Boolean valid = true;
 
-            //coord not empty
+            //check coord not empty
             if (board[move[0], move[1]] != 0)
             {
                 valid = false;
             }
 
-            //TODO: stone will be captured
+            //consider stones this move will capture (but don't update board)
+            int[,] hypotheticalCapture = (int[,])board.Clone();
+            checkCapture(hypotheticalCapture, move);
 
-            //TODO: move will create previous board state
+            //TODO: check if stone will be captured
+
+            //TODO: check if move will create previous board state
 
             return valid;
         }
 
         /// <summary>
-        /// Gets a list of all connected stones of the same color. Stones of the same color 
+        /// Gets a list of all stones connected to a given stone. Stones of the same color 
         /// which are directly up/down/left/right of each other (no diagonals) are connected, and
-        /// connectedness is transitive.
+        /// connectedness is transitive. Stones are always connected to themselves.
         /// </summary>
         /// <param name="stone">x coord, y coord, and color of the stone.</param>
-        /// <returns>int[ , ]: an array of [x, y] coords, each corresponding to a connected stone</returns>
-        private int[ , ] getConnected(int[] stone)
+        /// <returns>ArrayList: an array of [x, y] coords, each corresponding to a connected stone</returns>
+        private ArrayList getConnected(int[] stone)
+        {
+            ArrayList connected = new ArrayList();
+            connected.Add(stone);
+
+            int[] currStone = stone;
+            Stack stonesToCheck = new Stack();
+            stonesToCheck.Push(currStone);
+            while (stonesToCheck.Count != 0)
+            {
+                currStone = (int[])stonesToCheck.Pop();
+                Boolean[] adjacentsToCheck = { false, false, false, false };
+                Boolean[] adj = findAdjacents(currStone);
+                int x = currStone[0];
+                int y = currStone[1];
+
+                //the 4 cardinal directions
+                for (int dir = 0; dir < 4; dir++)
+                {
+                    //up
+                    if (dir == 0)
+                    {
+                        if (adj[dir] && !contains(connected, new int[] { x, y - 1 }))
+                        {
+                            connected.Add(new int[] { x, y - 1 });
+                            adjacentsToCheck[0] = true;
+                            stonesToCheck.Push(new int[] { x, y - 1 });
+                        }
+                    }
+
+                    //down
+                    if (dir == 1)
+                    {
+                        if (adj[dir] && !contains(connected, new int[] { x, y + 1 }))
+                        {
+                            connected.Add(new int[] { x, y + 1 });
+                            adjacentsToCheck[1] = true;
+                            stonesToCheck.Push(new int[] { x, y + 1 });
+                        }
+                    }
+
+                    //left
+                    if (dir == 2)
+                    {
+                        if (adj[dir] && !contains(connected, new int[] { x - 1, y }))
+                        {
+                            connected.Add(new int[] { x - 1, y });
+                            adjacentsToCheck[2] = true;
+                            stonesToCheck.Push(new int[] { x - 1, y });
+                        }
+                    }
+
+                    //right
+                    if (dir == 3)
+                    {
+                        if (adj[dir] && !contains(connected, new int[] { x + 1, y }))
+                        {
+                            connected.Add(new int[] { x + 1, y });
+                            adjacentsToCheck[3] = true;
+                            stonesToCheck.Push(new int[] { x + 1, y });
+                        }
+                    }
+                }
+
+            }
+            return connected;
+        }
+
+        /// <summary>
+        /// Gets a list of all liberties of a given stone on a given board. 
+        /// A liberty of a stone is an empty space next to the stone or 
+        /// any stone connected to that stone.
+        /// </summary>
+        /// <param name="stone">[x, y] corresponding to the stone</param>
+        /// <returns>an array of [x, y] coords which correspond to empty spaces
+        /// that are liberties of the given stone</returns>
+        private int[ , ] getLiberties(int[,] board, int[] stone)
         {
             return new int[0, 0];
         }
 
         /// <summary>
-        /// Gets a list of all liberties of a given stone. A liberty of a stone is an empty space 
-        /// next to the stone or any stone connected to that stone.
+        /// Determine which stones a move captures. A move captures a stone if it
+        /// removes all liberties of that stone. This method looks to the intersections
+        /// directly adjacent to it and if a stone of the opposite color is found, checks
+        /// the liberties of that stone. If it is 0, it and all stones connected to it
+        /// are captured.
         /// </summary>
-        /// <param name="stone">[x, y] corresponding to the stone</param>
-        /// <returns>an array of [x, y] coords which correspond to empty spaces
-        /// that are liberties of the given stone</returns>
-        private int[ , ] getLiberties(int[] stone)
+        /// <param name="board">The board on which to look for captures. We want this to 
+        /// be different from the global board because we want to figure out if a move 
+        /// will capture any pieces before committing that move to the global board.</param>
+        /// <param name="moveJustPlayed">[0]: x, [1]: y, [2]: color</param>
+        /// <returns></returns>
+        private int[ , ] checkCapture(int[ , ] board, int[] moveJustPlayed)
         {
+            board[moveJustPlayed[0], moveJustPlayed[1]] = moveJustPlayed[3];
+
             return new int[0, 0];
         }
-
 
         /// <summary>
         /// Method to be called when a capture is detected.
@@ -104,6 +191,125 @@ namespace Go.Shared.Models
         private void capture()
         {
 
+        }
+
+        /// <summary>
+        /// Helper method to prevent out of bounds indexing.
+        /// </summary>
+        /// <param name="location">The location in question</param>
+        /// <returns>[x, x, x, x]: x=false if the adjacent intersection 
+        /// is out of bounds, x=true otherwise. Corresponds
+        /// to "up/down/left/right". ie at location [0, 0], it will
+        /// return [false, true, false, true].</returns>
+        private Boolean[] bounds(int[] location)
+        {
+            Boolean[] bound = { false, false, false, false };
+
+            if (location[1] > 0)
+            {
+                bound[0] = true;
+            }
+            if (location[1] < board.GetLength(0)-1)
+            {
+                bound[1] = true;
+            }
+            if (location[0] > 0)
+            {
+                bound[2] = true;
+            }
+            if (location[0] < board.GetLength(1)-1)
+            {
+                bound[3] = true;
+            }
+
+            return bound;
+        }
+
+        /// <summary>
+        /// Helper method to find adjacent stones of the same color.
+        /// Functions similarly to bounds().
+        /// </summary>
+        /// <param name="location">The location in question</param>
+        /// <returns>[x, x, x, x] where x=false if there is no adjacent stone
+        /// of the same color, true otherwise. Corresponds to 
+        /// up/down/left/right</returns>
+        private Boolean[] findAdjacents(int[] location)
+        {
+            Boolean[] adjacents = { false, false, false, false };
+            int x = location[0];
+            int y = location[1];
+            Boolean[] b = bounds(location);
+            if (b[0] && board[x, y-1] == board[x, y])
+            {
+                adjacents[0] = true;
+            }
+            if (b[1] && board[x, y+1] == board[x, y])
+            {
+                adjacents[1] = true;
+            }
+            if (b[3] && board[x-1, y] == board[x, y])
+            {
+                adjacents[2] = true;
+            }
+            if (b[4] && board[x+1, y] == board[x, y])
+            {
+                adjacents[3] = true;
+            }
+            return adjacents;
+        }
+
+        /// <summary>
+        /// Helper method for getConnected(). 
+        /// </summary>
+        /// <param name="stone"></param>
+        /// <param name="list"></param>
+        /// <returns>True if the stone has an adjacent stone not in list,
+        /// false otherwise</returns>
+        private Boolean hasAdjacentsNotInList(int[] stone, ArrayList list)
+        {
+            Boolean[] adj = findAdjacents(stone);
+
+            if (adj[0] && !list.Contains(new int[stone[0], stone[1] - 1]))
+            {
+                return true;
+            }
+            if (adj[1] && !list.Contains(new int[stone[0], stone[1] + 1]))
+            {
+                return true;
+            }
+            if (adj[2] && !list.Contains(new int[stone[0] - 1, stone[1]]))
+            {
+                return true;
+            }
+            if (adj[3] && !list.Contains(new int[stone[0] + 1, stone[1]]))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Helper method to find if an ArrayList contains a specified
+        /// int[]. The built in .Contains() method cannot be used because
+        /// we do not want to look for object references to the specific int[]
+        /// but rather the values contained by the int[].
+        /// </summary>
+        /// <param name="arrayList"></param>
+        /// <param name="stone"></param>
+        /// <returns></returns>
+        private Boolean contains(ArrayList arrayList, int[] stone)
+        {
+            Boolean contains = false;
+            int[][] list = (int[][])arrayList.ToArray(typeof(int[]));
+            for (int i = 0; i < list.Length; i++)
+            {
+                if (list[i][0] == stone[0] && list[i][1] == stone[1])
+                {
+                    contains = true;
+                }
+            }
+            return contains;
         }
     }
 }
