f9a3cc8206d30c190a280d7a2a1568145471afa1
southard2 <61202965+southard2@users.noreply.github.com>
2021-11-02 20:43:39 -0500

    Added in 19x19 image, working on settings for 12x12, 9x9.

---
+++ Go/Shared/Forms/FormModels/BoardOptions.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+namespace Go.Shared.Forms.FormModels
+{
+    public class BoardOptions
+    {
+        [Required]
+        public String dimensions { get; set; } = "19";
+    }
+}
