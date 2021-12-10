1d86e5e6de8d00bfe5d35adb6f040f1a701eb5ca
Alexander Moon <ajmoon2@wisc.edu>
2021-12-10 14:36:43 -0600

    Begin BUnit testing

---
+++ Testing/UnitTests/Shared/Models/Forms/TestLogoutButton.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+using Blazored.LocalStorage;
+
+using Bunit;
+using Go;
+using Go.Shared.Forms;
+using Moq;
+using Xunit;
+
+namespace Testing.UnitTests.Shared.Models.Forms
+{
+    public class TestLogoutButton
+    {
+        [Fact]
+        public void TestClick()
+        {
+            using var context = new TestContext();
+            var component = context.RenderComponent<LogoutButton>();
+
+            Mock<ILocalStorageService> localStorageMock = new Mock<ILocalStorageService>();
+            localStorageMock.Setup(s => s.RemoveItemAsync(It.IsAny<String>(), It.IsAny<CancellationToken>()));
+
+            component.Find("headtext").Click();
+
+            localStorageMock.Verify((s => s.RemoveItemAsync(It.IsAny<String>(), It.IsAny<CancellationToken>())), Times.Once());
+        }
+    }
+}
