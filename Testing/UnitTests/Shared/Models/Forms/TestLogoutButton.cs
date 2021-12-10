cd26e0481592e403a26ae581098822b252008a6d
Alexander Moon <ajmoon2@wisc.edu>
2021-12-10 16:11:18 -0600

    Test logout button

--- Testing/UnitTests/Shared/Models/Forms/TestLogoutButton.cs
+++ Testing/UnitTests/Shared/Models/Forms/TestLogoutButton.cs
@@ -6,29 +6,31 @@ using System.Threading.Tasks;
 using System.Threading;
 
 using Blazored.LocalStorage;
+using Microsoft.Extensions.DependencyInjection;
 
 using Bunit;
-using Go;
 using Go.Shared.Forms;
 using Moq;
 using Xunit;
 
 namespace Testing.UnitTests.Shared.Models.Forms
-{
+{ 
     public class TestLogoutButton
     {
         [Fact]
         public void TestClick()
         {
+            Mock<ILocalStorageService> localStorageMock = new Mock<ILocalStorageService>();
+            localStorageMock.Setup(s => s.RemoveItemAsync(It.IsAny<String>(), It.IsAny<CancellationToken?>()));
+
+
             using var context = new TestContext();
+            context.Services.AddSingleton<ILocalStorageService>(localStorageMock.Object);
             var component = context.RenderComponent<LogoutButton>();
 
-            Mock<ILocalStorageService> localStorageMock = new Mock<ILocalStorageService>();
-            localStorageMock.Setup(s => s.RemoveItemAsync(It.IsAny<String>(), It.IsAny<CancellationToken>()));
+            component.Find(".headtext").Click();
 
-            component.Find("headtext").Click();
-
-            localStorageMock.Verify((s => s.RemoveItemAsync(It.IsAny<String>(), It.IsAny<CancellationToken>())), Times.Once());
+            localStorageMock.Verify((s => s.RemoveItemAsync(It.IsAny<String>(), It.IsAny<CancellationToken?>())), Times.Once());
         }
     }
 }
