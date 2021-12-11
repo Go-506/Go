a784af14519801d41083546d6af1b15ecbd52ebc
Joey <jaschuett@wisc.edu>
2021-12-11 13:43:23 -0600

    add images to lessons

--- Go/Shared/Models/LessonPage.cs
+++ Go/Shared/Models/LessonPage.cs
@@ -8,6 +8,7 @@ namespace Go.Shared.Models
     public class LessonPage
     {
         public string text { get; set; }
+        public string img { get; set; }
         public LessonPage()
         {
 
